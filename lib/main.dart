e01599906906f0dc18cc72de7fe3412f130fb6e4
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-25 23:01:05 +0100

    trying to implemen the theme in my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/app_theme.dart';
 import 'package:news_app/pages/home_screen.dart';
 
 void main() {
@@ -11,8 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
       home: HomeScreen(),
     );
   }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/pages/search_screen.dart';
 import 'package:news_app/providers/news_provider.dart';
+import 'package:news_app/providers/theme_provider.dart';
 import 'package:news_app/widgets/news_card.dart';
 import 'package:news_app/widgets/search_file.dart';
 
@@ -11,9 +13,10 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final news = ref.watch(newsProvider).newsModel;
     final isLoading = ref.watch(newsProvider).isLoading;
+    final appThemeState = ref.watch(appThemeStateNotifier);
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(190, 208, 207, 207),
+      backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.grey,
         title: const Text("Breaking News for Today"),
@@ -37,6 +40,25 @@ class HomeScreen extends ConsumerWidget {
                   ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          if (index == 1) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => SearchScreen()), // Replace with your search screen
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
       ),
     );
   }
