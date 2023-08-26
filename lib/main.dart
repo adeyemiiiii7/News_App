e73102676bda25c52cb5f6fa36765aad2127c1e9
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-26 11:01:07 +0100

    theme changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app/app_theme.dart';
 import 'package:news_app/pages/home_screen.dart';
+import 'package:news_app/providers/theme_provider.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp(),));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AppThemeState = ref.watch(appThemeStateNotifier);
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
