1c36a1b9e9de5cc83c4cca7646125ce97218580c
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-26 23:03:22 +0100

    added a tabs screen to switch screen

---
+++ lib/pages/tabs.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/pages/home_screen.dart';
+import 'package:news_app/pages/search_screen.dart';
+import 'package:news_app/providers/theme_provider.dart';
+
+class TabsScreen extends ConsumerStatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends ConsumerState<TabsScreen> {
+  int _selectedPageIndex = 0;
+  
+  void _selectedPage (int index){
+  setState(() {
+    _selectedPageIndex = index;
+  });
+  }
+  @override
+  Widget build(BuildContext context) { 
+    final appThemeState = ref.watch(appThemeStateNotifier);
+    Widget activePage = const HomeScreen();
+   if(_selectedPageIndex == 1) {
+    activePage = SearchScreen();
+   }
+   
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('changes'),
+      // ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+         backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
+         onTap: _selectedPage,
+          currentIndex: _selectedPageIndex,
+        //  (index) { if (index == 1) {
+        //     Navigator.of(context).push(
+        //       MaterialPageRoute(builder: (context) => SearchScreen()), // Replace with your search screen
+        //     );
+        //   }
+        // },
+        items: [
+          BottomNavigationBarItem(icon: 
+          Icon(Icons.home), 
+          label: 'Home',   
+           backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,),
+          BottomNavigationBarItem(icon: 
+          Icon(Icons.search_sharp), 
+          label: 'Discover',
+              backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
+          ),
+          
+        ]),
+    );
+  }
+}
