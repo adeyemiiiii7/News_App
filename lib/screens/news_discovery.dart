e4943ca18d84d075d6cace94ad714df1a45bee9f
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-27 22:19:32 +0100

    made a screen for discovery screen

---
+++ lib/screens/news_discovery.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/data/data_sources/discovery_options.dart';
+import 'package:news_app/models/news_model.dart';
+import 'package:news_app/providers/theme_provider.dart';
+import 'package:news_app/widgets/news_grid.dart';
+
+class NewsDiscoveryScreen extends ConsumerWidget {
+  const NewsDiscoveryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+     final appThemeState = ref.watch(appThemeStateNotifier);
+    return Scaffold(
+       backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
+      appBar: AppBar(
+         backgroundColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white12,
+        title: const Text('Pick A News Discovery'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for(final discovery in availableDiscovery)
+          NewsGridItem(discovery: discovery),
+        ],
+        )
+    );
+  }
+}
