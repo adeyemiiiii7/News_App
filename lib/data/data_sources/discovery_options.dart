4f1a0759f901f04d46ed64646ef715be85ba0f45
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-27 22:17:55 +0100

    made discovery item names

---
+++ lib/data/data_sources/discovery_options.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/news_model.dart';
+
+
+const availableDiscovery = [
+  Discovery(
+    id: 'c1',
+    title: 'Sports',
+    color: Colors.red,
+  ),
+  Discovery(
+    id: 'entertainment',
+    title: 'Entertainment',
+    color: Colors.blue,
+  ),
+  Discovery(
+    id: 'technology',
+    title: 'Technology',
+    color: Colors.green,
+  ),
+  Discovery(
+    id: 'politics',
+    title: 'Politics',
+    color: Colors.orange,
+  ),
+];
