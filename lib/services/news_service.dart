350eaec3d838122d0a76a9d0e7829b1b25974dbd
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-28 23:58:42 +0100

    new data fetch

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -15,7 +15,27 @@ class NewsService {
   }
 
   Future<dynamic> fetchNewsBySearch(String title) async {
-   var response = await _dio.get('everything?q=$title&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
+    var response = await _dio.get('everything?q=$title&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
     return response.data;
   }
+
+  Future<dynamic> fetchSportsNews() async {
+    var response = await _dio.get('news?language=en&category=sports&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
+    return response.data;
+  }
+  Future<dynamic> fetchEntertainment() async{
+     var response = await _dio.get('news?language=en&category=entertainment&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
+    return response.data;
+  }
+  Future<dynamic> fetchTechnology() async {
+     var response = await _dio.get('news?language=en&category=technology&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
+    return response.data;
+  }
+   Future<dynamic> fetchPolitics() async {
+     var response = await _dio.get('news?language=en&category=politics&apiKey=pub_28078ed31faab4e9faa15a14087c40df53ffa');
+    return response.data;
+  }
+  
+
 }
+
