8399a1fa1aa49716e78511767ac7213162ee57aa
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-30 23:27:03 +0100

    new screen

---
+++ lib/providers/discovery_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class DiscoveryList extends StatefulWidget {
+  const DiscoveryList({super.key});
+
+  @override
+  State<DiscoveryList> createState() => _DiscoveryListState();
+}
+
+class _DiscoveryListState extends State<DiscoveryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
