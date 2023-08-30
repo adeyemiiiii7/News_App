8beb250d9f91e139b164a9df89df448af2ae629e
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-30 23:25:26 +0100

    changes

--- lib/widgets/news_grid.dart
+++ lib/widgets/news_grid.dart
@@ -2,33 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app/models/news_model.dart';
 import 'package:news_app/providers/theme_provider.dart';
+import 'package:news_app/screens/entertainment_screen.dart';
+import 'package:news_app/screens/politics_screen.dart';
+import 'package:news_app/screens/sport_screen.dart';
+import 'package:news_app/screens/technology_screen.dart';
 
 
 class NewsGridItem extends ConsumerWidget {
-  const NewsGridItem({super.key,
-  required this.discovery});
-   final Discovery discovery;
+  const NewsGridItem({
+    Key? key,
+    required this.discovery,
+  }) : super(key: key);
+
+  final Discovery discovery;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-  final appThemeState = ref.watch(appThemeStateNotifier);
+    final appThemeState = ref.watch(appThemeStateNotifier);
+    
     return InkWell(
-         onTap: () {},
-      splashColor:  appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
+      onTap: () {
+        Future.microtask(() {
+          // Future.microtask, you're ensuring that the provider modifications are delayed until after 
+          // the widget tree has finished building, which should help resolve the error you were encountering.
+
+        if (discovery.id == 'sports') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const SportScreen(),
+            ),
+          );
+        } else if (discovery.id == 'entertainment') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const EntertainmentScreen(),
+            ),
+          );
+        } else if (discovery.id == 'politics') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const PoliticsScreen(),
+            ),
+          );
+        } else if (discovery.id == 'technology') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const TechnologyScreen(),
+            ),
+          );
+        }
+        },
+        );
+      },
+      splashColor: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
       borderRadius: BorderRadius.circular(16),
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            gradient: LinearGradient(
-              colors: [
-                discovery.color.withOpacity(0.55),
-                discovery.color.withOpacity(0.9),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            )),
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              discovery.color.withOpacity(0.55),
+              discovery.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Text(
-           discovery.title,
+          discovery.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: appThemeState.isDarkModeEnable ? Colors.grey[900] : Colors.white,
               ),
@@ -37,4 +83,3 @@ class NewsGridItem extends ConsumerWidget {
     );
   }
 }
-
