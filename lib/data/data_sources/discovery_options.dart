28e769ef50180e6836f217226272892e7978a36a
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-09-04 23:28:44 +0100

    added image path

--- lib/data/data_sources/discovery_options.dart
+++ lib/data/data_sources/discovery_options.dart
@@ -7,20 +7,24 @@ const availableDiscovery = [
     id: 'sports',
     title: 'Sports',
     color: Colors.red,
+    flagImagePath: 'https://onthegosports.com.au/cdn/shop/articles/sports.jpg?v=1627890142'
   ),
   Discovery(
     id: 'entertainment',
     title: 'Entertainment',
-    color: Colors.blue,
+    color: Colors.blue, 
+    flagImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI7Gl9CeoRMQ6koJWYzSIo0iFy8ahGqE_oyw&usqp=CAU',
   ),
   Discovery(
     id: 'technology',
     title: 'Technology',
     color: Colors.green,
+     flagImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkTFCdVSNIwhcySyDwTyLKhkpDNIJ7l2InNn2zvKaRQypTojweygzLiPFj87SFjBjtAzM&usqp=CAU',
   ),
   Discovery(
     id: 'politics',
     title: 'Politics',
-    color: Colors.orange,
+    color: Colors.orange, 
+    flagImagePath: 'https://th-i.thgim.com/public/incoming/st1r88/article65374975.ece/alternates/FREE_1200/09EPBS_GLOBAL%20POLITICS.jpg',
   ),
 ];
