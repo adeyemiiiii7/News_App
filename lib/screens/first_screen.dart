6e81b9363c35032ba6383147b7c6cceb87c8a0e7
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-09-06 23:18:33 +0100

    added  NEW screen to display app logo

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/providers/theme_provider.dart';
+import 'package:news_app/screens/auth.dart';
+import 'package:news_app/screens/home_screen.dart';
+
+class FirstScreen extends ConsumerStatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends ConsumerState<FirstScreen> {
+  // final PageController _pageController = PageController(initialPage: 0);
+  // final List<Widget> _screens = [
+  //   const AuthScreen(),
+  // ];
+  void _continue(){
+  Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AuthScreen(),
+  ),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C1919),
+      body: Stack(
+        children: [
+          // PageView for swiping between screens
+         
+          // Content on top of the PageView
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/image/applogo.png',
+                  width: 300,
+                ),
+                const SizedBox(height: 30),
+                OutlinedButton.icon(
+                  onPressed: _continue,
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  icon: const Icon(Icons.arrow_forward_ios_sharp,
+                  size: 40,),
+                  label: const Text('CONTINUE'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
