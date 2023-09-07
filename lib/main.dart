60ebd2b202aca88a038bbc3ea6d5ca3dce978192
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-09-07 23:00:54 +0100

    added firebase propeties and used  a stream builder  that builds itself based on the latest snapshot of interaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app/app_theme.dart';
+import 'package:news_app/screens/auth.dart';
 import 'package:news_app/screens/first_screen.dart';
+import 'package:news_app/providers/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:news_app/screens/home_screen.dart';
 import 'package:news_app/screens/tabs.dart';
-import 'package:news_app/providers/theme_provider.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp(),));
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //when app freezes after firbase intialization
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+        const ProviderScope(
+      child: MyApp(),
+    ));
 }
 
 class MyApp extends ConsumerWidget {
@@ -21,8 +34,14 @@ class MyApp extends ConsumerWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
-      home: const FirstScreen()
+      home: StreamBuilder(stream:  FirebaseAuth.instance.authStateChanges(),builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+             return const TabsScreen();
+            }
+
+            return const AuthScreen();
+          }),);
      //  const TabsScreen(),
-    );
+  
   }
 }
